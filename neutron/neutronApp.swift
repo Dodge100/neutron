5256f61c7107bd12dff302a8a3e0edddf02d55f0
Dodge1 <varung2235@gmail.com>
2025-10-31 18:26:40 -0700

    Initial Commit

---
+++ neutron/neutronApp.swift
@@ -0,0 +1,17 @@
+//
+//  neutronApp.swift
+//  neutron
+//
+//  Created by Varun G on 10/31/25.
+//
+
+import SwiftUI
+
+@main
+struct neutronApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
